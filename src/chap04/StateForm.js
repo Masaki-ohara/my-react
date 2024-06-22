c909b653b6d5c63aafeedc21e082b29ca7acba1f
Masaki-Ohara <masakingu921@gmail.com>
2024-06-23 00:03:33 +0900

    form作成

---
+++ src/chap04/StateForm.js
@@ -0,0 +1,42 @@
+import { useState } from 'react';
+
+export default function StateForm() {
+    // フォームとして扱う値をStateとして宣言
+    const [form, setForm] = useState({
+        name: '山田太郎',
+        age: 18
+    });
+    // フォーム要素の変更をstateに反映
+    const handleForm = e => {
+        setForm({
+            ...form,
+            [e.target.name]: e.target.value
+        });
+    };
+    //送信ボタンでログにメッセージを出力
+    const show = () => {
+        console.log(`こんにちは、${form.name} (${form.age} 歳)さん!`);
+    };
+
+    return (
+        <form>
+            {/* State値を個々のフォーム要素に割り当て */}
+             <div>
+                 <label htmlFor="name" >名前:</label>
+                 <input id="name" name="name" type="text"
+                   onChange={handleForm} value={form.name} />
+             </div>
+             <div>
+             <label htmlFor="age" >名前:</label>
+                 <input id="age" name="age" type="text"
+                   onChange={handleForm} value={form.age} />
+             </div>
+             <div>
+                 <button type="button" onClick={show}>
+                     送信
+                 </button>
+             </div>
+             <p>こんにちは、{form.name} ({form.age} 歳）さん！</p>
+        </form>
+    )
+}
